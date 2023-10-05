2b2ec2cfffd59d8379dc402b0fee13c23876af33
Alex McComas <alex.mccomas4@yahoo.com>
2023-10-05 17:12:32 -0400

    Added an html page that is integrated with javascript that has multiple random functions

--- itis3135/scripts/defaultscripts.js
+++ itis3135/scripts/defaultscripts.js
@@ -21,52 +21,142 @@ function numberToPolygon()
     if (number == 1)
     {
         number = Math.abs(number);
-        alert();
+        alert("Henagon");
     }
     else if (number == 2)
     {
         number = Math.abs(number);
-        alert();
+        alert("Digon");
     }
     else if (number == 3)
     {
         number = Math.abs(number);
-        alert();
+        alert("Trigon");
     }
     else if (number == 4)
     {
         number = Math.abs(number);
-        alert();
+        alert("Tetragon");
     }
     else if (number == 5)
     {
         number = Math.abs(number);
-        alert();
+        alert("Pentagon");
     }
     else if (number == 6)
     {
         number = Math.abs(number);
-        alert();
+        alert("Hexagon");
     }
     else if (number == 7)
     {
         number = Math.abs(number);
-        alert();
+        alert("Heptagon");
     }
     else if (number == 8)
     {
         number = Math.abs(number);
-        alert();
+        alert("Octagon");
     }
     else if (number == 9)
     {
         number = Math.abs(number);
-        alert();
+        alert("Enneagon");
     }
     else if (number == 10)
     {
         number = Math.abs(number);
-        alert();
+        alert("Decagon");
     }
+}
 
+function hitADriver() 
+{
+    let distance = Math.random() * 350;
+    distance = Math.floor(distance);
+
+    if (distance < 100 && distance > 0)
+    {
+        document.getElementById("driverDistance").innerHTML = "You hit your driver " + distance + "yards. You might wanna hit another one :(";
+    }
+    else if (distance < 200 && distance >= 100)
+    {
+        document.getElementById("driverDistance").innerHTML = "You hit your driver " + distance + 
+            "yards. Not a bad drive, but you're gonna have a long second shot.";
+    }
+    else if (distance < 250 && distance >= 200)
+    {
+        document.getElementById("driverDistance").innerHTML = "You hit your driver " + distance + "yards. Pretty good drive.";
+    }
+    else if (distance < 300 && distance >= 250)
+    {
+        document.getElementById("driverDistance").innerHTML = "You hit your driver " + distance + "yards. You got a hold of that one, nice drive!";
+    }
+    else if (distance < 350 && distance >= 300)
+    {
+        document.getElementById("driverDistance").innerHTML = "You hit your driver " + distance + 
+            "yards. Wow! You crushed that one, I would check the green to see if you made it!";
+    }
+}
+
+function dadJoke()
+{
+    let jokeNumber = Math.random() * 5;
+    jokeNumber = Math.floor(jokeNumber);
+
+    switch (jokeNumber)
+    {
+        case 1 :
+            document.getElementById("generateDadJoke").innerHTML = "My neighbor gave me a new roof for free. He said it was on the house.";
+            break;
+            
+        case 2:
+            document.getElementById("generateDadJoke").innerHTML = "What do you call a boomerang that doesn't come back? A stick.";
+            break;
+        
+        case 3: 
+            document.getElementById("generateDadJoke").innerHTML = "Dad did you get a haircut? 'No I got them all cut!'";
+            break;
+
+        case 4: 
+            document.getElementById("generateDadJoke").innerHTML = "Why did the elephant quit his job? He was working for peanuts.";
+            break;
+
+        case 5:
+            document.getElementById("generateDadJoke").innerHTML = "What are the least expensive type of teeth? Buck teeth!";
+            break;
+
+        default:
+            document.getElementById("generateDadJoke").innerHTML = "No dad joke for you";
+    }
+}
+
+function multiplyTwoNumbers()
+{
+    let number1 = document.getElementById("number1").value;
+    let number2 = document.getElementById("number2").value;
+
+    let multipliedNumber = number1 * number2;
+
+    document.getElementById("multiplication").innerHTML = "Your multiplied number: " + multipliedNumber;
+}
+
+function makeAPutt()
+{
+    let number = Math.random() * 2;
+
+    number = Math.floor(number);
+
+    if (number == 1)
+    {
+        document.getElementById("putt").innerHTML = "You made it!!!";
+    }
+    else if (number == 2)
+    {
+        document.getElementById("putt").innerHTML = "You didn't make it :((";
+    }
+    else 
+    {
+        document.getElementById("putt").innerHTML = "Go ahead and putt already!";
+    }
 }
